e47171bc8c27b4bef65773236e0ec95ccf6a1a42
Dacosta <dacostadesarrollo@gmail.com>
2023-03-16 23:40:56 -0500

    oneSignal ionic

---
+++ android/app/src/main/java/com/pushapp/devdacosta/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pushapp.devdacosta;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import OneSignal from 'onesignal-cordova-plugin';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(platform: Platform) {
+
+    platform.ready().then(() => {
+      this.OneSignalInit();
+    });
+
+  }
+  // Call this function when your app starts
+  OneSignalInit(): void {
+    // Uncomment to set OneSignal device logging to VERBOSE  
+    OneSignal.setLogLevel(6, 0);
+    
+    // NOTE: Update the setAppId value below with your OneSignal AppId.
+    OneSignal.setAppId("f324d34b-747c-46e2-a25b-e0b7fafd72f9");
+    OneSignal.setNotificationOpenedHandler(function(jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    });
+
+    // Prompts the user for notification permissions.
+    //    * Since this shows a generic native prompt, we recommend instead using an In-App Message to prompt for notification permission (See step 7) to better communicate to your users what notifications they will get.
+    OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+        console.log("User accepted notifications: " + accepted);
+    });
+  }
+
 }
